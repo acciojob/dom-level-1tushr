79b675298489cc8ebb28684cb3aa59505047a9d1
Tushar Verma <110741618+1tushr@users.noreply.github.com>
2023-04-28 20:49:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Get the element with ID "level"
+var element = document.getElementById("level");
+
+// Initialize the level counter to 0
+var level = 0;
+
+// Loop through the parent elements of the element until we reach the top level
+while (element.parentNode) {
+  element = element.parentNode;
+  level++;
+}
+
+// Display the level in an alert box
+alert("The level of the element is: " + level);
